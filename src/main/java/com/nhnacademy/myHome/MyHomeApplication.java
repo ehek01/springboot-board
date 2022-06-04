a4aa35ead4dcca6595c12efef9d44fc723db80f2
= <whdgus0515@gmail.com>
2022-06-05 01:09:34 +0900

    init

---
+++ src/main/java/com/nhnacademy/myHome/MyHomeApplication.java
@@ -0,0 +1,13 @@
+package com.nhnacademy.myHome;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyHomeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyHomeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nhnacademy/myHome/MyHomeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nhnacademy.myHome;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyHomeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
