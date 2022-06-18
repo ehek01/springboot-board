a091c07a901644d65ac4e30f1ae144fcb8f00e54
Dodari <whdgus0515@gmail.com>
2022-06-18 14:19:03 +0900

    6->1:39

--- src/main/java/com/nhnacademy/myhome/controller/BoardController.java
+++ src/main/java/com/nhnacademy/myhome/controller/BoardController.java
@@ -3,6 +3,8 @@ package com.nhnacademy.myhome.controller;
 import com.nhnacademy.myhome.entity.Board;
 import com.nhnacademy.myhome.repository.BoardRepository;
 import java.util.List;
+
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -10,13 +12,10 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequiredArgsConstructor
 @RequestMapping("/board")
 public class BoardController {
     private final BoardRepository boardRepository;
-
-    public BoardController(BoardRepository boardRepository) {
-        this.boardRepository = boardRepository;
-    }
 
     @GetMapping("/list")
     public String list(Model model) {
@@ -25,5 +24,4 @@ public class BoardController {
 
         return "board/list";
     }
-    
 }
