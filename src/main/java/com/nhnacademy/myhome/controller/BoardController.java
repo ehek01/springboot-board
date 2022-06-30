8daedbf74deda2eed8f7c7016fcd41005fc340f8
ehek01 <whdgus0515@gmail.com>
2022-07-01 00:18:09 +0900

    6강 완료. TODO issue 있음.

--- src/main/java/com/nhnacademy/myhome/controller/BoardController.java
+++ src/main/java/com/nhnacademy/myhome/controller/BoardController.java
@@ -4,18 +4,22 @@ import com.nhnacademy.myhome.entity.Board;
 import com.nhnacademy.myhome.repository.BoardRepository;
 import java.util.List;
 
+import com.nhnacademy.myhome.validator.BoardValidator;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
 
 @Controller
 @RequiredArgsConstructor
 @RequestMapping("/board")
 public class BoardController {
     private final BoardRepository boardRepository;
+    private final BoardValidator boardValidator;
 
     @GetMapping("/list")
     public String list(Model model) {
@@ -23,5 +27,26 @@ public class BoardController {
         model.addAttribute("boards", boards);
 
         return "board/list";
+    }
+
+    @GetMapping("/form")
+    public String form(Model model, @RequestParam(required = false) Long id) {
+        if (id == null) {
+            model.addAttribute("board", new Board());
+        } else {
+            boardRepository.findById(id)
+                            .ifPresent(board -> model.addAttribute("board", board));
+        }
+        return "board/form";
+    }
+
+    @PostMapping("/form")
+    public String greetingSubmit(@Valid Board board, BindingResult bindingResult) {
+        boardValidator.validate(board, bindingResult);
+        if (bindingResult.hasErrors()) { // TODO 6 : custom validator 는 동작을 하는데, entity 에 해둔 validation 은 적용이 안된다 왜일까..
+            return "board/form";
+        }
+        boardRepository.save(board);
+        return "redirect:/board/list";
     }
 }
--- src/main/java/com/nhnacademy/myhome/entity/Board.java
+++ src/main/java/com/nhnacademy/myhome/entity/Board.java
@@ -4,14 +4,23 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import lombok.Data;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+import lombok.Getter;
+import lombok.Setter;
 
 @Entity
-@Data
+@Getter
+@Setter
 public class Board {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @NotNull
+    @Size(min=2, max=30, message = "제목은 2자이상 30자 이하입니다.")
     private String title;
+
     private String content;
 }
